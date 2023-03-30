adb5f344869450bd3fa06596737c59525c2f6ba7
Денис Комарьков <denis.komarkov@gmail.com>
2023-03-30 04:05:04 +0300

    format processes[-windows].go

--- processes-windows.go
+++ processes-windows.go
@@ -8,16 +8,16 @@ import (
 )
 
 func killProcessGroup(id int) {
-	logInfo("killing process " + strconv.FormatInt(int64(id), 10) + " with 'taskkill'...", false);
-	cmd := exec.Command("taskkill", "/T", "/F", "/PID", strconv.FormatInt(int64(id), 10));
-	err := cmd.Run();
+	logInfo("killing process "+strconv.FormatInt(int64(id), 10)+" with 'taskkill'...", false)
+	cmd := exec.Command("taskkill", "/T", "/F", "/PID", strconv.FormatInt(int64(id), 10))
+	err := cmd.Run()
 	if err != nil {
-		logWarning("failed to kill process " + strconv.FormatInt(int64(id), 10) + ":\n\t" + err.Error());
-	}else{
-		logInfo("\tdone", false);
+		logWarning("failed to kill process " + strconv.FormatInt(int64(id), 10) + ":\n\t" + err.Error())
+	} else {
+		logInfo("\tdone", false)
 	}
 }
 
 func configProcess() {
-	
+
 }
--- processes.go
+++ processes.go
@@ -8,15 +8,15 @@ import (
 )
 
 func killProcessGroup(id int) {
-	logInfo("killing process " + strconv.FormatInt(int64(id), 10) + "...", false);
-	err := syscall.Kill(-id, syscall.SIGKILL);
+	logInfo("killing process "+strconv.FormatInt(int64(id), 10)+"...", false)
+	err := syscall.Kill(-id, syscall.SIGKILL)
 	if err != nil {
-		logWarning("failed to kill process " + strconv.FormatInt(int64(id), 10) + ":\n\t" + err.Error());
-	}else{
-		logInfo("\tdone", false);
+		logWarning("failed to kill process " + strconv.FormatInt(int64(id), 10) + ":\n\t" + err.Error())
+	} else {
+		logInfo("\tdone", false)
 	}
 }
 
 func configProcess() {
-	childProcess.SysProcAttr = &syscall.SysProcAttr{Setpgid: true};
+	childProcess.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 }
